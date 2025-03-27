01216b643d4c91c1eb4cce8f78d6d154f392263b
VitorrAmorim <vi08tor@gmail.com>
2025-03-26 22:39:05 -0300

    aula 2

--- aula02/components/DimensoesFixas.js
+++ aula02/components/DimensoesFixas.js
@@ -1,11 +1,15 @@
-import { View } from "react=native";
+import { View } from "react-native";
 const DimensoesFixas = () => {
   return (
-    <View>
-      <View style={{ width: 50, height: 50, backgroundColor: "powerblue" }}></View>
-      <View style={{ width: 100, height: 100, backgroundColor: "red" }}></View>
-      <View style={{ width: 150, height: 150, backgroundColor: "green" }}></View>
+    <View style={{ width: "100%", height: "100%", flexDirection: "row"  }}>
+      <View style={{ width: 50, height: 50, backgroundColor: "powderblue" }}></View>
+      <View style={{ width: 50, height: 50, backgroundColor: "skyblue" }}></View>
+      <View style={{ width: 50, height: 50, backgroundColor: "steelblue" }}></View>
+      <View style={{ width: 50, height: 50, backgroundColor: "powderblue" }}></View>
+      <View style={{ width: 50, height: 50, backgroundColor: "skyblue" }}></View>
+      <View style={{ width: 50, height: 50, backgroundColor: "steelblue" }}></View>
     </View>
   )
 }
+
 export default DimensoesFixas;
