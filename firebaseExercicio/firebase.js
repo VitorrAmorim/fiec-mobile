4aea7f34b378357f53bfe6cf203289537c07b201
Vitor Amorim <vi08tor@gmail.com>
2025-06-03 20:30:47 -0300

    Update firebase.js

--- firebaseExercicio/firebase.js
+++ firebaseExercicio/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeCO75uaQSZc0_TCwGj9Cd4QlvwqFyBy4",
-  authDomain: "fir-exercicio-f364e.firebaseapp.com",
-  projectId: "fir-exercicio-f364e",
-  storageBucket: "fir-exercicio-f364e.firebasestorage.app",
-  messagingSenderId: "69029749636",
-  appId: "1:69029749636:web:9ee348260c8ba5b552f3d1"
+// API
 };
 
 const app = initializeApp(firebaseConfig);
